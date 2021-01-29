5fee2b40a2da9d3a882af66324116f8c5393d85c
Elliot-Bl <32825844+Elliot-Bl@users.noreply.github.com>
2021-01-28 19:26:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,12 @@ from picamera import PiCamera
 import tensorflow.compat.v1 as tf
 import argparse
 import sys
+from PIL import Image
 
-tf.disable_v2_behavior()
+import tflite_runtime.interpreter as tflite
+
+
+# tf.disable_v2_behavior()
 
 # Set up camera constants
 IM_WIDTH = 1280
@@ -45,58 +49,58 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 # Name of the directory containing the object detection module we're using
-MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+# MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
 
 # Grab path to current working directory
 CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph .pb file, which contains the model that is used
 # for object detection.
-PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+# PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
 
 # Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
+# PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
 
 # Number of classes the object detector can identify
-NUM_CLASSES = 90
+# NUM_CLASSES = 90
 
 ## Load the label map.
 # Label maps map indices to category names, so that when the convolution
 # network predicts `5`, we know that this corresponds to `airplane`.
 # Here we use internal utility functions, but anything that returns a
 # dictionary mapping integers to appropriate string labels would be fine
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
-                                                            use_display_name=True)
-category_index = label_map_util.create_category_index(categories)
+# label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+# categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+#                                                             use_display_name=True)
+# category_index = label_map_util.create_category_index(categories)
 
 # Load the Tensorflow model into memory.
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
-    sess = tf.Session(graph=detection_graph)
+# detection_graph = tf.Graph()
+# with detection_graph.as_default():
+#     od_graph_def = tf.GraphDef()
+#     with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+#         serialized_graph = fid.read()
+#         od_graph_def.ParseFromString(serialized_graph)
+#         tf.import_graph_def(od_graph_def, name='')
+#
+#     sess = tf.Session(graph=detection_graph)
 
 # Define input and output tensors (i.e. data) for the object detection classifier
 
 # Input tensor is the image
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+# image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
 
 # Output tensors are the detection boxes, scores, and classes
 # Each box represents a part of the image where a particular object was detected
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+# detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
 
 # Each score represents level of confidence for each of the objects.
 # The score is shown on the result image, together with the class label.
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+# detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+# detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
 # Number of objects detected
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+# num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 # Initialize frame rate calculation
 frame_rate_calc = 1
@@ -111,6 +115,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 # detection loop twice, and made one work for Picamera and the other work
 # for USB.
 
+mapper = {0:'anger', 1:'disgust', 2:'fear', 3:'happiness', 4: 'sadness', 5: 'surprise', 6: 'neutral'}
 ### Picamera ###
 if camera_type == 'picamera':
     # Initialize Picamera and grab reference to the raw capture
@@ -120,7 +125,7 @@ if camera_type == 'picamera':
     rawCapture = PiRGBArray(camera, size=(IM_WIDTH, IM_HEIGHT))
     rawCapture.truncate(0)
 
-    for frame1 in camera.capture_continuous(rawCapture, format="rgb", use_video_port=True):
+    for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
         t1 = cv2.getTickCount()
 
@@ -128,26 +133,47 @@ if camera_type == 'picamera':
         # i.e. a single-column array, where each item in the column has the pixel RGB value
         frame = np.copy(frame1.array)
         frame.setflags(write=1)
-        # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_expanded = np.expand_dims(frame, axis=0)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).resize((48,48))
+        frame_expanded = np.expand_dims(frame_rgb, axis=0)
 
-        # Perform the actual detection by running the model with the image as input
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
+        # Load the TFLite model and allocate tensors.
+        interpreter = tflite.Interpreter(model_path="emotions.tflite")
+        interpreter.allocate_tensors()
+
+        # Get input and output tensors.
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        # Test the model on random input data.
+        # input_shape = input_details[0]['shape']
+        # input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+
+        interpreter.set_tensor(input_details[0]['index'], [frame_expanded])
+
+        interpreter.invoke()
+
+        # cv2_imshow(image * 255)
+
+        # The function `get_tensor()` returns a copy of the tensor data.
+        # Use `tensor()` in order to get a pointer to the tensor.
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+
+        # need to show predition on the screen
+        print("Guess: ", mapper[np.where(output_data[0] == np.max(output_data[0]))[0][0]],
+              "(%.02f%%)" % np.max(output_data[0] * 100))
 
         # Draw the results of the detection (aka 'visulaize the results')
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            frame,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=8,
-            min_score_thresh=0.40)
-
-        cv2.putText(frame, "FPS: {0:.2f}".format(frame_rate_calc), (30, 50), font, 1, (255, 255, 0), 2, cv2.LINE_AA)
+        # vis_util.visualize_boxes_and_labels_on_image_array(
+        #     frame,
+        #     np.squeeze(boxes),
+        #     np.squeeze(classes).astype(np.int32),
+        #     np.squeeze(scores),
+        #     category_index,
+        #     use_normalized_coordinates=True,
+        #     line_thickness=8,
+        #     min_score_thresh=0.40)
+        #
+        # cv2.putText(frame, "FPS: {0:.2f}".format(frame_rate_calc), (30, 50), font, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
         # All the results have been drawn on the frame, so it's time to display it.
         cv2.imshow('Object detector', frame)
